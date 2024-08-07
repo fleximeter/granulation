8cc03595942ae8c1677986e87679d0d836c68748
Jeff Martin <jeffreymartin@outlook.com>
2024-08-06 20:24:41 -0500

    added tagger

---
+++ tag.py
@@ -0,0 +1,36 @@
+"""
+File: tag.py
+
+This is a grain tagger.
+"""
+
+import os
+import platform
+import sqlite3
+
+MAC = "/Users/jmartin50/recording"
+ARGON = "/Users/jmartin50/recording"
+PC = "D:\\recording"
+SYSTEM = platform.system()
+
+if SYSTEM == "Darwin":
+    DB = os.path.join(MAC, "data/grains.sqlite3")    
+elif SYSTEM == "Linux":
+    DB = os.path.join(ARGON, "data/grains.sqlite3")
+else:
+    DB = os.path.join(PC, "data/grains.sqlite3")
+
+db = sqlite3.connect(DB)
+cursor = db.cursor()
+
+cursor.execute("""
+               SELECT id FROM grains WHERE
+               (file LIKE '%metal%')
+               """)
+items = cursor.fetchall()
+cursor.executemany("""
+                   INSERT INTO tags (grain_id, tag)
+                   VALUES (?, 'metal');
+                   """, items)
+
+db.commit()
